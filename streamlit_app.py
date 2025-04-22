474f3c7c1b7f868cf2d2218185b1e61982f6f95d
avadhut_17 <avadhootd.in@mouritech.com>
2025-04-22 12:50:48 +0530

    Add pandas dependency and convert Snowflake dataframe to pandas for display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
-
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
@@ -13,8 +13,12 @@ st.write(
 
 conn = st.connection("snowflake")
 session = conn.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(data=pd_df, use_container_width=True)
+st.stop()
 
 
 name_on_order = st.text_input("Name of Smoothie: ")
